3998ac91ff65e718b9cb92cace545fd2975df260
suleman <iam.suleman.m@gmail.com>
2023-06-06 19:24:44 +0500

    Adding user data on database using form and post-methed.

--- app.js
+++ app.js
@@ -12,45 +12,12 @@ app.set('view engine' , 'ejs');
 
 app.use(express.static("./Css"));
 app.use(express.static("./images"));
+app.use(express.urlencoded({extended : true}));
 
 
 app.get("/" , (req , res)=>{
     res.render("index");
 })
-
-// mongo_db routing
-app.get("/abc" , (req , res) => {
-    const allData = async () => {
-        try {
-            const userData1 = new User({
-                user_name : "Suleman",
-                user_age : 18,
-                user_job : "Computer science"
-            })
-            const userData2 = new User({
-                user_name : "majid",
-                user_age : 22,
-                user_job : "Computer science"
-            })
-            const userData3 = new User({
-                user_name : "abdul rehman",
-                user_age : 28,
-                user_job : "game develpor"
-            })
-            const userData4 = new User({
-                user_name : "wassem",
-                user_age : 33,
-                user_job : "Computer science"
-            })
-            const result = await User.insertMany([userData1 , userData2 , userData3 , userData4]);
-            res.send(result);
-            res.end()
-        } catch (error) {
-            console.log(error)
-        }   
-    }
-    allData()
-});
 
 app.get("/about" , (req , res)=>{
     res.redirect("/user");
@@ -60,6 +27,14 @@ app.get("/user" , (req , res) =>{
     User.find().then((result) => res.render("about" , {user : result})).catch((err) => console.log(err));
 })
 
+app.get("/user/user-form" , (req , res) => {
+    res.render("userForm");
+    res.end();
+})
+app.post("/user" , (req , res) => {
+    const userInformation = new User(req.body);
+    userInformation.save().then((result) => res.redirect("/user")).catch((err) => console.log(err))
+})
 
 app.get("/contect-us" , (req , res)=>{
     res.render("contect");
